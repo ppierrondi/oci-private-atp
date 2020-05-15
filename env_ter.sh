0b2cc7c991302f5c6c525a4f041a2114cf094113
PPierrondi <61242429+ppierrondi@users.noreply.github.com>
2020-05-15 16:32:09 +0100

    Update env_ter.sh

--- env_ter.sh
+++ env_ter.sh
@@ -1,13 +1,13 @@
 ### oci
-export TF_VAR_tenancy_ocid="ocid1.tenancy.oc1..aaaaaaaadeoj3n5gq7dn7wt32cc4l6mzdopbhfumluhbp5haw4smiepeld7q"
-export TF_VAR_user_ocid="ocid1.user.oc1..aaaaaaaab7em6zux2kpm4tkxgk53tnylkra7jyfs6xdikufdrtktncwtsztq"
-export TF_VAR_compartment_ocid="ocid1.compartment.oc1..aaaaaaaaywbfhbbnfgyf47pq4uikio5qhqqpxxw4c34l2wpjfiiligm5jrna"
-export TF_VAR_fingerprint="ad:c9:c2:d1:0b:f3:00:cb:1d:81:e8:cc:36:d2:07:39"
-export TF_VAR_private_key_path="/Users/paulopierrondi/.oci/oci_api_key.pem"
+export TF_VAR_tenancy_ocid="ocid1.tenancy.oc1..."
+export TF_VAR_user_ocid="ocid1.user.oc1..."
+export TF_VAR_compartment_ocid="ocid1.compartment.oc1..."
+export TF_VAR_fingerprint="..."
+export TF_VAR_private_key_path="....oci/oci_api_key.pem"
 
 
-export TF_VAR_private_key_oci="/Users/paulopierrondi/.ssh/id_rsa"
-export TF_VAR_public_key_oci="/Users/paulopierrondi/.ssh/id_rsa.pub"
+export TF_VAR_private_key_oci="home/opc/.ssh/id_rsa"
+export TF_VAR_public_key_oci="home/opc/.ssh/id_rsa.pub"
 export TF_VAR_ssh_public_key=$(cat ~/.ssh/id_rsa.pub)
 export TF_VAR_ssh_private_key=$(cat ~/.ssh/id_rsa)
 export TF_VAR_region="uk-london-1"
